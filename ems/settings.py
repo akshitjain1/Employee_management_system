11068369216cde7040cd705856b8913d249a5c24
Akshit Jain <akshitjainonly1@gmail.com>
2026-02-13 10:52:43 +0530

    Change Setting python file

--- ems/settings.py
+++ ems/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_panel',
+    'hr_module',
+    'employee_module',
+    'hr_module'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'ems.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
